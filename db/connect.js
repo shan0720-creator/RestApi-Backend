e879898b05701de9d7fe7f1a30241c692a4e6696
shantanu sahay <71556064+shan0720-creator@users.noreply.github.com>
2023-03-27 16:20:52 +0530

    restapi

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+
+const connectDB = (uri)=>{
+    console.log("Database Connected");
+    return mongoose.connect(uri,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+}
+
+
+module.exports = connectDB;
